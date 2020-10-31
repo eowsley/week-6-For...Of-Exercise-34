1ce6b6e0c8751b308fc750a4586c4644395cf5f1
eowsley <66394238+eowsley@users.noreply.github.com>
2020-10-30 18:15:51 -0700

    Update index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const numbers = [1,2,3,4,5,6,7,8,9]; //DON'T CHANGE THIS LINE PLEASE!
+
+// use a for...of Loop over the array and print out the square of each value:
+for (let i of numbers) {
+    console.log(i*i);
+}
+
